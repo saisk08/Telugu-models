fb04ffbf6c70c03e59f90c5b53f3175944ced2c1
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-28 00:59:26 +0530

    Add resnet and densenet layers

---
+++ src/lib/utils/layers.py
@@ -0,0 +1,35 @@
+from torch import nn
+import torch
+
+
+def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+        nn.BatchNorm(out_channels),
+        nn.ReLU()
+    )
+
+
+class ResBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = conv_block(channels, channels)
+        self.conv2 = conv_block(channels, channels)
+
+    def forward(self, x): return x + self.conv2(self.conv1(x))
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = conv_block(channels, channels)
+        self.conv2 = conv_block(channels, channels)
+
+    def forward(self, x): return torch.cat(x, self.conv2(self.conv1(x)))
+
+
+def res_block(channels, dense=False):
+    if not dense:
+        return ResBlock(channels)
+    else:
+        return DenseBlock(channels)
