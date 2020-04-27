c1165d481d0a4d7782b15a03b2f20de56ed067e2
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-27 22:26:56 +0530

    Update dataset class functions to work fo any size of data

--- src/lib/datasets/siamese.py
+++ src/lib/datasets/siamese.py
@@ -2,47 +2,54 @@ from torch.utils.data import Dataset
 from pathlib import Path
 import pickle
 import torch
+import numpy as np
 
 
 class Siamese(Dataset):
-    '''Dataset class for telugu chars; siamese  learning without rdms'''
+    '''Dataset class for telugu chars; siamese learning without rdms'''
 
-    def __init__(self, train=False, val=False, test=False, transforms=None, size=None):
+    def __init__(self, mode=None, transforms=None, size=None):
         super().__init__()
         self.path = Path('../../../data/siamese')
         if size is None:
-            self.f = open(self.path / 'chars.pkl', 'rb')
+            f = open(self.path / 'chars.pkl', 'rb')
         else:
-            self.f = open(self.path / 'char_{}.pkl'.format(size), 'rb')
-        self.dump = pickle.load(self.f)
-        self.f.close()
-        (self.train_x, self.train_y), (self.val_x,
-                                       self.val_y), (self.test_x, self.test_y) = self.dump
-        self.train = train
-        self.val = val
-        self.test = test
+            f = open(self.path / 'char_{}.pkl'.format(size), 'rb')
+        self.train, self.val, self.test = pickle.load(f)
+        f.close()
+        self.mode = mode
         self.tfms = transforms
 
     def __len__(self):
-        if self.train:
-            return self.train_y.shape[0]
-        if self.val:
-            return self.val_y.shape[0]
-        if self.test:
-            return self.test_y.shape[0]
+        if self.mode == 'train':
+            return np.multiply(*self.train[[0, 2]])
+        if self.mode == 'val':
+            return np.multiply(*self.val[[0, 2]])
+        if self.mode == 'test':
+            return np.multiply(*self.test[[0, 2]])
 
     def __getitem__(self, index):
         if torch.is_tensor(index):
-            index = index.tolist()
+            index = index.numpy()
+        if self.mode == 'train':
+            cat = index // self.train.shape[2]
+            ids = index // self.train.shape[0]
+            img1, img2, label = self.train[cat, 0, ids], \
+                self.train[cat, 1, ids], cat
+        if self.mode == 'val':
+            cat = index // self.val.shape[2]
+            ids = index // self.val.shape[0]
+            img1, img2, label = self.val[cat, 0, ids], \
+                self.val[cat, 1, ids], cat
+        if self.mode == 'test':
+            cat = index // self.test.shape[2]
+            ids = index // self.test.shape[0]
+            img1, img2, label = self.test[cat, 0, ids], \
+                self.test[cat, 1, ids], cat
 
-        if self.train:
-            img, label = self.train_x[index], self.train_y[index]
-        if self.val:
-            img, label = self.train_x[index], self.train_y[index]
-        if self.test:
-            img, label = self.train_x[index], self.train_y[index]
-
+        img1, img2 = img1.reshape(-1, 32, 32), img2.reshape(-1, 32, 32)
         if self.tfms is not None:
-            img = self.tfms(img)
+            img1 = self.tfms(img1)
+            img2 = self.tfms(img2)
 
-        return img, label
+        return img1, img2, label
