960ee3434b27a44dcb23de7e5130a89439dd2abe
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-29 15:47:44 +0530

    Add model save location

--- src/lib/utils/io.py
+++ src/lib/utils/io.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def save(model, p): return torch.save(model.state_dict(), p)
+def save(model, p): return torch.save(model.state_dict(), p / 'model.pth')
 
 
-def load(model, p): return model.load_state_dict(torch.load(p))
+def load(model, p): return model.load_state_dict(torch.load(p / 'model.pth'))
