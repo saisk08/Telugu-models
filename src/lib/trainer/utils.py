a774d91466a97816b2cc8aced92b25e6a79784a9
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-29 15:48:29 +0530

    Add exp_id as an instance variable of BaseTrainer
    - Remove exp_id as an argument to the do_experiment function
    - Modify init of all derived classes
    - This change allows for easy propagation of exp_id, which helps in easy
    creation of the directory structure for maintaing logs

--- src/lib/trainer/utils.py
+++ src/lib/trainer/utils.py
@@ -1,35 +1,36 @@
 from trainer import create_trainer
 
 
-def get_trainers(size, mode):
-    return [create_trainer(mode, 'normal', size=size),
-            create_trainer(mode, 'resent', size=size),
-            create_trainer(mode, 'dense', size=size)]
+def get_trainers(size, mode, exp_id):
+    return [create_trainer(exp_id, mode, 'normal', size=size),
+            create_trainer(exp_id, mode, 'resent', size=size),
+            create_trainer(exp_id, mode, 'dense', size=size)]
 
 
-def sizes(sl, mode):
-    return [get_trainers(x, mode) for x in sl]
+def sizes(sl, mode, exp_id):
+    return [get_trainers(x, mode, exp_id) for x in sl]
 
 
 class BaseTrainer():
-    def __init__(self):
+    def __init__(self, exp_id):
         self.size_list = [30, 50, 100, 150, 200]
+        self.exp_id = exp_id
 
-    def do_experiments(self, exp_id):
+    def do_experiments(self):
         raise NotImplementedError
 
 
 class BatchTrainer(BaseTrainer):
-    def __init__(self, lr, epoch_list):
-        super().__init__()
+    def __init__(self, lr, epoch_list, exp_id):
+        super().__init__(self, exp_id)
         self.epoch_list = epoch_list
 
         for epoch in self.epoch_list:
-            sup_trainers = sizes(self.size_list, 'supervised')
-            sia_trainers = sizes(self.size_list, 'siamese')
+            sup_trainers = sizes(self.size_list, 'supervised', self.exp_id)
+            sia_trainers = sizes(self.size_list, 'siamese', self.exp_id)
             self.batch = [sup_trainers, sia_trainers]
 
-    def do_experiments(self, exp_id):
+    def do_experiments(self):
         for epoch in self.epoch_list:
             for trainers in self.batch:
                 for t in trainers:
@@ -37,30 +38,30 @@ class BatchTrainer(BaseTrainer):
 
 
 class SingleTrainer(BaseTrainer):
-    def __init__(self, lr, epochs):
-        super().__init__()
+    def __init__(self, lr, epochs, exp_id):
+        super().__init__(self, exp_id)
         self.epochs = epochs
 
         for epoch in self.epoch_list:
-            sup_trainers = sizes(self.size_list, 'supervised')
-            sia_trainers = sizes(self.size_list, 'siamese')
+            sup_trainers = sizes(self.size_list, 'supervised', self.exp_id)
+            sia_trainers = sizes(self.size_list, 'siamese', self.exp_id)
             self.batch = [sup_trainers, sia_trainers]
 
-    def do_experiments(self, exp_id):
+    def do_experiments(self):
         for trainers in self.batch:
             for t in trainers:
                 t.fit(self.epochs)
 
 
 class BasicTrainer(BaseTrainer):
-    def __init__(self, lr, epochs, mode, model_type):
-        super().__init__()
+    def __init__(self, lr, epochs, mode, model_type, exp_id):
+        super().__init__(self, exp_id)
         self.epochs = epochs
 
         for epoch in self.epoch_list:
-            self.batch = [create_trainer(mode, model_type, size=x)
+            self.batch = [create_trainer(self.exp_id, mode, model_type, size=x)
                           for x in self.size_list]
 
-    def do_experiments(self, exp_id):
+    def do_experiments(self):
         for t in self.batch:
             t.fit(self.epochs)
