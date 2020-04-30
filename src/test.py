4d8ddeff2d0dc22920f458f745087d3925937532
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-30 16:54:15 +0530

    Update tests

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import _init_paths
 from trainer.runners import BasicTrainer
 
-t = BasicTrainer(1e-3, 10, 'supervised', 'resnet', 'test_01')
+t = BasicTrainer(1e-3, 10, 'supervised', 'resnet', 'test_03')
 t.do_experiments()
