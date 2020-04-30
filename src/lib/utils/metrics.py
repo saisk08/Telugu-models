21b42b2830742c2ac4a591069ff1f172729924fd
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-30 10:53:19 +0530

    Add accuracy function when rmse is used as loss function

--- src/lib/utils/metrics.py
+++ src/lib/utils/metrics.py
@@ -5,6 +5,11 @@ def cross_acc(out, yb): return (
     torch.argmax(out, dim=1) == yb).float().mean()
 
 
+def rmse_acc(out, yb):
+    loss = RMSELoss()
+    return 1 - loss(out, yb)
+
+
 class RMSELoss(torch.nn.Module):
     def __init__(self, eps=1e-6):
         super().__init__()
