e7ca7cb889853e75f52a7597595026b8f5cc28ee
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-30 10:56:21 +0530

    Add new functions
    - Add a function to create tests and load model based on the parameters
    - Modify the load function to take a paramter tuning to know if the last
    few weights must be discarded

--- src/lib/trainer/tests.py
+++ src/lib/trainer/tests.py
@@ -1,4 +1,44 @@
 import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from datasets.supervised import Supervised
+from datasets.rdm import Rdms
+from networks import resnet, densenet, normal
+from utils.data import WrappedDataLoader
+from utils import io, logger, metrics
+
+
+def create_tester(exp_id, mode, model_type, bs=32, size=30):
+    tfms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize()
+    ])
+    # Create a trainer depending on the mode
+    if mode == 'supervised':
+        test_ds = Supervised('test', transforms=tfms, size=size)
+        loss_func = torch.nn.CrossEntropyLoss()
+        if model_type == 'resnet':
+            model = resnet.Telnet()
+        elif model_type == 'desne':
+            model = densenet.Telnet()
+        elif model_type == 'normal':
+            model = normal.Telnet()
+        metric = metrics.cross_acc
+    elif mode == 'siamese':
+        test_ds = Rdms('test', transforms=tfms, size=size)
+        loss_func = metrics.RMSELoss()
+        if model_type == 'resnet':
+            model = resnet.Siameserdm()
+        elif model_type == 'desne':
+            model = densenet.Siameserdm()
+        elif model_type == 'normal':
+            model = normal.Siameserdm()
+        metric = metrics.rmse_acc
+    log = logger.Logger(exp_id, mode, model_type, size)
+    test_dl = WrappedDataLoader(DataLoader(
+        test_ds, batch_size=bs * 2, num_workers=4))
+    model = io.load(model, log.full_path, size)
+    return Tester(model, test_dl, loss_func, log, metric)
 
 
 class Tester():
--- src/lib/utils/io.py
+++ src/lib/utils/io.py
@@ -5,5 +5,6 @@ def save(model, p, size): return torch.save(
     model.state_dict(), p / 'model_{}.pth'.format(size))
 
 
-def load(model, p, size): return model.load_state_dict(
-    torch.load(p / 'model_{}.pth'.format(size)))
+def load(model, p, size, tuning=False):
+    return model.load_state_dict(
+        torch.load(p / 'model_{}.pth'.format(size)))
