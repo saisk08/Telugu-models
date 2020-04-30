90d9bf5cd061f1bca3bf4271b3bebee5a2f46f94
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-30 10:37:05 +0530

    Move RMSELoss to utils

--- src/lib/utils/metrics.py
+++ src/lib/utils/metrics.py
@@ -3,3 +3,12 @@ import torch
 
 def cross_acc(out, yb): return (
     torch.argmax(out, dim=1) == yb).float().mean()
+
+
+class RMSELoss(torch.nn.Module):
+    def __init__(self, eps=1e-6):
+        super().__init__()
+        self.mse = torch.nn.MSELoss()
+        self.eps = eps
+
+    def forward(self, yhat, y): return torch.sqrt(self.mse(yhat, y) + self.eps)
