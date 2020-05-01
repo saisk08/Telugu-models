c6ec6cc98d1a42ab5e0652a38e810076c296b823
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-01 17:11:51 +0530

    Update parameter list

--- src/lib/utils/io.py
+++ src/lib/utils/io.py
@@ -7,7 +7,7 @@ def save(model, p, size): return torch.save(
     model.state_dict(), p / 'model_{}.pth'.format(size))
 
 
-def load(model, sup_id, model_type, size):
-    p = Path(Path.cwd, '../../../Logs', sup_id, model_type)
+def load(model, sup_id, model_type, size, mode='supervised'):
+    p = Path(Path.cwd, '../../../Logs', sup_id, mode, model_type)
     return model.load_state_dict(
         torch.load(p / 'model_{}.pth'.format(size)))
