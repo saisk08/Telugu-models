1ba5066f9b5603b5bdb2af8648c960b749f384b4
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-01 16:56:31 +0530

    Modify load function

--- src/lib/utils/io.py
+++ src/lib/utils/io.py
@@ -1,10 +1,13 @@
 import torch
+from pathlib import Path
+import os
 
 
 def save(model, p, size): return torch.save(
     model.state_dict(), p / 'model_{}.pth'.format(size))
 
 
-def load(model, p, size, tuning=False):
+def load(model, sup_id, model_type, size):
+    p = Path(Path.cwd, '../../../Logs', sup_id, model_type)
     return model.load_state_dict(
         torch.load(p / 'model_{}.pth'.format(size)))
