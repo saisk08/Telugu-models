eaf8d81fa9a250c35a2d5be3f7a353c09e99d006
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-01 16:46:33 +0530

    Add fine tuner class

--- src/lib/trainer/trainer.py
+++ src/lib/trainer/trainer.py
@@ -131,3 +131,49 @@ class Trainer():
 
         self.logger.done()
         io.save(self.model, self.logger.full_path, self.logger.size)
+
+
+class Finetuner():
+    def __init__(self, model, train_dl, valid_dl, loss_func, lr, logger,
+                 metric, mode):
+        self.model = model
+        self.train_dl = train_dl
+        self.valid_dl = valid_dl
+        self.loss_func = loss_func
+        self.logger = logger
+        self.metric = metric
+        self.lr = lr
+        self.logger.log_summary(self.model, (1, 32, 32))
+        self.opt = optim.SGD(self.model.parameters(), lr=lr,
+                             momentum=0.9, weight_decay=0.02)
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+        self.model.to(self.device)
+
+    def fit(self, epochs):
+        for epoch in range(epochs):
+            self.model.train()
+            for xb, yb in self.train_dl:
+                loss = self.loss_func(self.model(xb), yb)
+                loss.backward()
+                self.opt.step()
+                self.opt.zero_grad()
+
+            self.model.eval()
+            with torch.no_grad():
+                tot_loss, tot_acc = 0., 0.
+                for xb, yb in self.valid_dl:
+                    pred = self.model(xb)
+                    temp = self.loss_func(pred, yb)
+                    tot_loss += temp
+                    tot_acc += self.metric(pred,
+                                           yb) if self.metric else 1 - temp
+            nv = len(self.valid_dl)
+            val_loss = tot_loss / nv
+            acc = tot_acc / nv
+            print('Epoch: {}, train loss: {:.6f}, val loss: {:.6f}, Acc: {:.6f}'.format(
+                epoch + 1, loss, val_loss, acc * 100))
+            self.logger.log([loss, val_loss, acc])
+
+        self.logger.done()
+        io.save(self.model, self.logger.full_path, self.logger.size)
