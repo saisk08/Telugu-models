53356a7e2f8a3269f4dad0278cf2f28dd4a71fe1
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-01 17:12:38 +0530

    Add create_examiner utility function

--- src/lib/trainer/tests.py
+++ src/lib/trainer/tests.py
@@ -8,40 +8,29 @@ from utils.data import WrappedDataLoader
 from utils import io, logger, metrics
 
 
-def create_tester(exp_id, mode, model_type, bs=32, size=30):
+def create_examiner(exp_id, mode, model_type, bs=32, size=30):
     tfms = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize()
     ])
-    # Create a trainer depending on the mode
-    if mode == 'supervised':
-        test_ds = Supervised('test', transforms=tfms, size=size)
-        loss_func = torch.nn.CrossEntropyLoss()
-        if model_type == 'resnet':
-            model = resnet.Telnet()
-        elif model_type == 'desne':
-            model = densenet.Telnet()
-        elif model_type == 'normal':
-            model = normal.Telnet()
-        metric = metrics.cross_acc
-    elif mode == 'siamese':
-        test_ds = Rdms('test', transforms=tfms, size=size)
-        loss_func = metrics.RMSELoss()
-        if model_type == 'resnet':
-            model = resnet.Siameserdm()
-        elif model_type == 'desne':
-            model = densenet.Siameserdm()
-        elif model_type == 'normal':
-            model = normal.Siameserdm()
-        metric = metrics.rmse_acc
+    # Create a examiner depending on the mode
+    test_ds = Supervised('test', transforms=tfms, size=size)
+    metric = metrics.cross_acc
+    if model_type == 'resnet':
+        model = resnet.Telnet()
+    elif model_type == 'desne':
+        model = densenet.Telnet()
+    elif model_type == 'normal':
+        model = normal.Telnet()
+    loss_func = torch.nn.CrossEntropyLoss()
     log = logger.Logger(exp_id, mode, model_type, size)
     test_dl = WrappedDataLoader(DataLoader(
         test_ds, batch_size=bs * 2, num_workers=4))
-    model = io.load(model, log.full_path, size)
-    return Tester(model, test_dl, loss_func, log, metric)
+    model = io.load(model, exp_id, model_type, size, mode)
+    return Examiner(model, test_dl, loss_func, log, metric)
 
 
-class Tester():
+class Examiner():
     def __init__(self, model, test_dl, logger, metric):
         self.model = model
         self.test_dl = test_dl
