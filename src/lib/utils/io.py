2c73ccd3779071f2341a6fe24297329f8f8a6a8b
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-06 21:33:54 +0530

    Fix return value
    Since the model variable is passed by reference, we do not need to
    return the output of the io.load function.

--- src/lib/utils/io.py
+++ src/lib/utils/io.py
@@ -9,8 +9,8 @@ def save(model, p, size): return torch.save(
 def load(model, exp_id, model_type, size, mode='supervised', version=None):
     if version is not None:
         p = Path(Path.cwd(), '../Logs',
-                 exp_id, mode, version, model_type)
+                 exp_id, mode, str(version), model_type)
     else:
         p = Path(Path.cwd(), '../Logs', exp_id, mode, model_type)
-    return model.load_state_dict(
-        torch.load(p / 'model_{}.pth'.format(size)))
+    model.load_state_dict(torch.load(p / 'model_{}.pth'.format(size)))
+    return
