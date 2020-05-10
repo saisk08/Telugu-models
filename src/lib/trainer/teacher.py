d52f4af0a0abad4f8d65afa25d64dfa4270a488b
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-10 19:37:54 +0530

    Update methods of Finetuner

--- src/lib/trainer/teacher.py
+++ src/lib/trainer/teacher.py
@@ -76,35 +76,24 @@ class Fineteacher():
         else:
             return [sizes]
 
-    def add_experiment(self, mode, model_type, size, lr, bs, epochs):
-        if mode == 'supervised':
-            exp = {'mode': mode, 'model_type': model_type, 'size': size,
-                   'lr': lr, 'bs': bs,
-                   'epochs': epochs, 'version': None}
-            self.exps.append(exp)
-            return
-        exp1 = {'mode': mode,
-                'model_type': model_type, 'size': size, 'lr': lr, 'bs': bs,
-                'version': 1,  'epochs': epochs}
-        exp2 = {'mode': mode,
-                'model_type': model_type, 'size': size, 'lr': lr, 'bs': bs,
-                'version': 2,  'epochs': epochs}
+    def add_experiment(self, model_type, size, lr, bs, epochs):
+        exp1 = {
+            'model_type': model_type, 'size': size, 'lr': lr, 'bs': bs,
+            'version': 1,  'epochs': epochs}
+        exp2 = {
+            'model_type': model_type, 'size': size, 'lr': lr, 'bs': bs,
+            'version': 2,  'epochs': epochs}
         self.exps.append(exp1)
         self.exps.append(exp2)
 
-    def add_supervised(self, model_type, size, lr, bs, epochs):
+    def add_all(self, model_type, size, lr, bs, epochs):
         for mt in self.get_types(model_type):
             for s in self.get_sizes(size):
-                self.add_experiment('supervised', mt, s, lr, bs, epochs)
-
-    def add_siamese(self, model_type, size, lr, bs, epochs):
-        for mt in self.get_types(model_type):
-            for s in self.get_sizes(size):
-                self.add_experiment('siamese', mt, s, lr, bs, epochs)
+                self.add_experiment(mt, s, lr, bs, epochs)
 
     def do_exps(self):
         for e in self.exps:
-            t = create_finetuner(self.exp_id, e['mode'],
-                                 e['model_type'], lr=e['lr'], bs=e['bs'],
-                                 size=e['size'], version=e['version'])
+            t = create_finetuner(self.exp_id, e['model_type'], lr=e['lr'],
+                                 bs=e['bs'], size=e['size'],
+                                 version=e['version'])
             t.fit(e['epochs'])
