5bf2117fd032ba75c443ab0505127d988177611f
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-10 19:53:08 +0530

    Add experiments

---
+++ exp1.py
@@ -0,0 +1,18 @@
+import _init_paths
+from trainer.teacher import Teacher, Fineteacher
+
+
+'''
+Run experiments with batch side of 32
+Use L1/RD loss for Siamese
+No similar images shown
+'''
+exp_name = 'exp1-batch32'
+
+t = Teacher(exp_name)
+t.add_siamese('all', 'all', 1e-3, 32, 10)
+t.do_exps()
+
+s = Fineteacher(exp_name)
+s.add_all('all', 'all', 5e-2, 32, 10)
+s.do_exps()
