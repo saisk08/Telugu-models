0a22b961c2da57dd2d9bfbc59bab97b431123ec3
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-11 17:08:18 +0530

    Add new experiment

--- src/exp2.py
+++ src/exp2.py
@@ -3,16 +3,26 @@ from trainer.teacher import Teacher, Fineteacher
 
 
 '''
-Run experiments with batch side of 32
+Run experiments with batch side of 64
 Use L1/RD loss for Siamese
 No similar images shown
 '''
-exp_name = 'exp2-batch32'
+exp_name = 'exp2-batch64'
 
 t = Teacher(exp_name)
-t.add_all('all', 'all', 1e-3, 32, 10)
+t.add_all('all', 'all', 1e-3, 64, 10)
 t.do_exps()
 
 s = Fineteacher(exp_name)
-s.add_all('all', 'all', 1e-2, 32, 10)
+s.add_all('all', 'all', 1e-2, 64, 10)
+s.do_exps()
+
+exp_name = 'exp3-batch64-lr'
+
+t = Teacher(exp_name)
+t.add_all('all', 'all', 5e-3, 64, 10)
+t.do_exps()
+
+s = Fineteacher(exp_name)
+s.add_all('all', 'all', 5e-2, 64, 10)
 s.do_exps()
