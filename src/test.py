1d68d1e0c48457e0fa786613a467a4aa4972e562
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-11 22:31:12 +0530

    Update tests

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from trainer.teacher import Teacher
 
 
 t = Teacher('new_test2')
-t.add_supervised('dense', 'all', 1e-3, 32, 10)
+t.add_siamese('resnet', '30', 1e-3, 32, 10)
 t.do_exps()
 
 # s = create_finetuner('fine', 'new_test1', 'densenet', 2, lr=5e-2, size=100)
